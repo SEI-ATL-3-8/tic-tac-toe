66f04ec0a9b7aae4e8e4d48a58f2a7e464de2b39
Derek Ensign <derekensign@gmail.com>
2021-03-14 20:09:21 -0500

    created helper functions for squares being taken

--- script.js
+++ script.js
@@ -1,107 +1,86 @@
-console.log('script is linked')
 
 // setTimeout(myAlert, 32000)
 
 const allGrids = document.querySelectorAll('.grid-square')
-
-let turnNbr = 0;
-let maxTurns = 9;
-// let xPlays = [];
-// let oPlays = [];
 
 document.getElementById("reset").addEventListener("click", (event) => {
     for (let i = 0; i < allGrids.length; i++){
         document.getElementById('oimg' + i).classList.add('hidden')
         document.getElementById('ximg' + i).classList.add('hidden')
     }
-    turnNbr = 0;
-    setTurn()
+    evaluateBoard()
 })
 
-const evaluateBoard = () =>
-{
-    for (let j = 0; j < allGrids.length; j++){
+const isTaken = (squareId) => {
+    return isX(squareId) || isO(squareId)
+}
 
-        turnNbr = 0
+const isX = (squareId) => {
+    return !document.getElementById('ximg' + squareId).classList.contains('hidden')
+}
 
-        if (!document.getElementById('oimg' + j).classList.contains('hidden') ||
-            !document.getElementById('ximg' + j).classList.contains('hidden'))
-        {turnNbr++}
+const isO = (squareId) => {
+    return !document.getElementById('oimg' + squareId).classList.contains('hidden')
+}
 
-        // if(!document.getElementById('oimg' + j).classList.contains('hidden')){
-        //     oPlays[j] = true
-        // }
-
-        // if(!document.getElementById('ximg' + j).classList.contains('hidden')){
-        //     xPlays[j] = true
-        // }
-
+const checkForEndOfGame = (turnNbr) => {
+    if (turnNbr >= allGrids.length){
+        console.log('out of turns, game over')
+        return
     }
-    console.log('Turn number is' + turnNbr)
-    return turnNbr
+//if (check for win) {
+    // if the same player has any win type, they win
+    // win types
+    // horizontal top
+    // horizontal middle
+    // horizontal bottom
+    // vertical left
+    // vertical middle
+    // vertical right
+    // forward slash
+    // back slash
+    // else draw
+//}
+}
 
-        
+const evaluateBoard = () => {
+    turnNbr = 0
+    for (let i = 0; i < allGrids.length; i++){
+        if (isTaken(i)){
+            turnNbr++
+        }
+    }
+    console.log('Turn number is ' + turnNbr)
+    setTurn(turnNbr)
+    checkForEndOfGame(turnNbr)
+    return turnNbr;
     // go thru allgrids and count number of turns
     // if beginning of game, say something about that
     // check for win
     // if end of game, say soemthing about that
 }
 
-
-const setTurn = () => {
-
+const setTurn = (turnNbr) => {
     let turn = turnNbr % 2 === 0 ? "X" : "O"
-    console.log('boyfriend ' + turn + '\'s turn')
     document.getElementById("turntxt").innerHTML = "It is " + turn + "'s turn."
-    
 }
 
-turnNbr = evaluateBoard()
-setTurn() // Setting turn at beginning of game
-
+evaluateBoard()
 for (let i = 0; i < allGrids.length; i++) {
     allGrids[i].addEventListener('click', (event) => {
-        
-        
-        console.log(turnNbr)
-        
-        if (turnNbr >= maxTurns){return}
-
-        if (!document.getElementById('ximg' + i).classList.contains('hidden') || 
-            !document.getElementById('oimg' + i).classList.contains('hidden')){
+        if (isTaken(i)){
             console.log('boyfriend' + i + ' is off the table')
             return
         }
-        
         if (turnNbr % 2 === 0){
-            console.log('got em x ' + turnNbr)
+            console.log('X to square ' + i)
             document.getElementById('ximg' + i).classList.remove('hidden')
         }
         else if (turnNbr % 2 === 1){
-            console.log('got em o ' + turnNbr)
+            console.log('O to square ' + i)
             document.getElementById('oimg' + i).classList.remove('hidden')
         }
-
-        
-        
-        // if (check for win)
-        // {
-        //     // if the same player has any win type, they win
-        //     // win types
-        //     // horizontal top
-        //     // horizontal middle
-        //     // horizontal bottom
-        //     // vertical left
-        //     // vertical middle
-        //     // vertical right
-        //     // forward slash
-        //     // back slash
-        //     // else draw
-        // }
-        
-        //evaluateBoard()
-        turnNbr++
-        setTurn() // setting turn after move
+        turnNbr = evaluateBoard()
     }
 );}
 
